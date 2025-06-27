77c17f8d9be7cf3e99c38d62aa56024108708ac5
Vishal K S <141414590+ksvishal07@users.noreply.github.com>
2025-06-27 21:43:08 +0530

    Delete Week1_Design principles & Patterns/FactoryMethodPattern directory

--- Week1_Design principles & Patterns/FactoryMethodPattern/Code/EmailNotificationFactory.java
+++
@@ -1,8 +0,0 @@
-
-
-public class EmailNotificationFactory extends NotificationFactory {
-    @Override
-    public Notification createNotification() {
-        return new EmailNotification();
-    }
-}
